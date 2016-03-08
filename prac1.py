fb0c8b5489b0f2810a73bf19be48dc9116ae8db0
Rose <rosehsu47@gmail.com>
2016-03-08 23:52:23 +0800

    hw1

---
+++ prac1.py
@@ -0,0 +1,5 @@
+
+# -*- coding: utf-8-*-
+bigcan="MERMAID"
+
+print bigcan
---
+++ w3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8-*-e
+A=44
+B=42
+C=40
+D="你好"
+E="拉拉拉"
+F="""
+"""
+
+print "A+B=",(A+C)
+print A,B,C,D,E,F
